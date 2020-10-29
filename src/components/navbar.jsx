e5b44abf2771210437f8000d791a729891fe459e
Jian Lin <wahjian.lin@gmail.com>
2020-10-28 21:47:41 -0400

    Removed the home navbar highlight

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,7 @@ class Navbar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
+            <li className="nav-item">
               <a className="nav-link" href="/">
                 Home <span className="sr-only">(current)</span>
               </a>
